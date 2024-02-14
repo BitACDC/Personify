6198189b9c6c62a8180e43713d7469c8686f67b0
BirdBrain34 <lanceatendido34@gmail.com>
2024-02-14 19:40:03 +0800

    Update: Added comments on signup.js

--- PRESONIFY(UPDATED)/signup.js
+++ PRESONIFY(UPDATED)/signup.js
@@ -1,10 +1,10 @@
 export function handleSignUp() {
-    const createAccConfirmButton = document.getElementById("CreateAccConfirm");
+    const createAccConfirmButton = document.getElementById("CreateAccConfirm");//gets values from button
 
     if (createAccConfirmButton) {
         createAccConfirmButton.addEventListener("click", async function () {
-            const getUsername = document.getElementById("Uname").value;
-            const getPassword = document.getElementById("CreatePass").value;
+            const getUsername = document.getElementById("Uname").value; //gets username
+            const getPassword = document.getElementById("CreatePass").value; //gets password
 
             try {
                 const response = await fetch('http://localhost:3000/api/v1/auth/register', {
@@ -13,10 +13,10 @@ export function handleSignUp() {
                         'Content-Type': 'application/json'
                     },
                     body: JSON.stringify({ username: getUsername, password: getPassword }),
-                });
+                });//sets up values for username and password
 
                 const token = await response.text();
-                console.log('Response:', token);
+                console.log('Response:', token);//status update on response 
             } catch (error) {
                 console.error('Failed to create user. Error:', error);
             }
