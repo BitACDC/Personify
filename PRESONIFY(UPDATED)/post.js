8fc7f678d6ac38990106bddf21d485f7fc37c661
RomelTibay <154564461+RomelTibay@users.noreply.github.com>
2024-02-14 20:57:49 +0800

    Made makePost API

---
+++ PRESONIFY(UPDATED)/post.js
@@ -0,0 +1,21 @@
+export async function CreatePost() {
+    var post = document.getElementById("PostInput").value;
+    let token = localStorage.getItem("token");
+
+    const res = await fetch('http://localhost:3000/api/v1/posts', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        },
+        body: JSON.stringify({
+            "content": post
+        })
+    });
+
+    if (res.ok) {
+        console.log('successful');
+    } else {
+        console.log('failed');
+    }
+}
